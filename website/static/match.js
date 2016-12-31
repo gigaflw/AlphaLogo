c52548a26ad07d759f9d5efb7422961bcabeb604
hgs1217 <mf20080540@126.com>
2017-01-01 00:05:02 +0800

    [前端] 关于搜索结果页面以及高级搜索板的美化
    -搜索结果页面的图片显示美化
    -高级搜索板的界面美化
    -修复了一些hover特效会导致页面产生细微变化的bug
    -高级搜索实际功能还未完成，期待下一个版本更新

--- website/static/match.js
+++ website/static/match.js
@@ -10,14 +10,20 @@
 var divSearch = document.getElementById("search");
 var divMatch = document.getElementById("match");
 var divSeniorSearch = document.getElementById("senior");
-var idBrowse = document.getElementById("browse");
-var idLogo = document.getElementById("logo");
-var idTextfield = document.getElementById("textfield");
-var idTypeSearch = document.getElementById("type_search");
 var idInputSearch = document.getElementById("input_search");
 var idInputMatch = document.getElementById("input_match");
 var idSeniorSearchPanel = document.getElementById("senior_search_panel");
 
 window.addEventListener('load', function(){
     console.log('远看黄山黑黝黝,上面小来下面大,若将黄山倒过来,上面大来下面小');
+    modeMatch();
 });
+
+function modeMatch() {
+    divSearch.className = "linkTitle";
+    divMatch.className = "currentLinkTitle";
+    divSeniorSearch.style.display = "none";
+    idInputSearch.style.display = "none";
+    idInputMatch.style.display = "block";
+    idSeniorSearchPanel.style.display = "none";
+}
