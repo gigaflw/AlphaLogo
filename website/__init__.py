c39056a7b19d872d2fe3c190f8ee16f40649bdc3
GigaFlower <a_gigantic_flower@yeah.net>
2017-01-07 11:58:25 +0800

    folder structure changed

--- website/__init__.py
+++ website/__init__.py
@@ -2,18 +2,18 @@
 # @Author: GigaFlower
 # @Date:   2016-12-22 20:23:50
 # @Last Modified by:   GigaFlower
-# @Last Modified time: 2017-01-04 15:15:16
+# @Last Modified time: 2017-01-07 11:12:27
 
 from flask import Flask
 
 from website.views import bp
-from website.database import init_db, db
+from website.database import db
 from website.core import init_searcher
 
 def create_app():
     app = Flask(__name__)
     app.secret_key = "God_is_dog"
-    init_db()
+    db.init()
     init_searcher()
 
     @app.before_request
---
+++ website/database/schema.sql
@@ -0,0 +1,13 @@
+DROP TABLE IF EXISTS LOGOS;
+CREATE TABLE LOGOS
+(   
+    IND           INTEGER         PRIMARY KEY,
+    FILENAME      VARCHAR(16)     NOT NULL,
+    ENT_NAME      VARCHAR(32)     NOT NULL,
+    INFO          VARCHAR(1024)   NOT NULL,
+    THEME_COLORS  VARCHAR(128),
+    THEME_WEIGHTS VARCHAR(32),
+    S             REAL,
+    V             REAL
+    -- TODO: not thrifty to store s,v deliberately
+);
