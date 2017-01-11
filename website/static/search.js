f86ce7390944b09ca8cc1edcb4e89eb5fbeae919
YujiePan <panyujie97@126.com>
2017-01-11 23:12:19 +0800

    merge conflict

--- website/static/search.js
+++ website/static/search.js
@@ -14,11 +14,34 @@ var idTypeSearch = document.getElementById("typeSearch");
 var idInputSearch = document.getElementById("inputSearch");
 var idInputMatch = document.getElementById("inputMatch");
 var idAdvancedSearchPanel = document.getElementById("advancedSearchPanel");
+var divResultContainer = document.getElementById("resultContainer");
+var idCurrentPointer = document.getElementById("currentPointer");
+
+var idTriangleLeft = document.getElementById("triangleLeft");
+var idTriangleRight = document.getElementById("triangleRight");
+var idResultPart1 = document.getElementById("resultPart1");
+var idResultPart2 = document.getElementById("resultPart2");
+var idResultPart3 = document.getElementById("resultPart3");
+
+var logoNum = parseInt(idTriangleRight.getAttribute("dataLogoNum"));
 
 window.addEventListener('load', function(){
     console.log('远看黄山黑黝黝,上面小来下面大,若将黄山倒过来,上面大来下面小');
+    resultPartInitialization();
     modeSearch();
 });
+
+idTriangleLeft.addEventListener("click", function() {
+    if (idTriangleLeft.className == "triangleLeft") {
+        previousPage();
+    }
+})
+
+idTriangleRight.addEventListener("click", function() {
+    if (idTriangleRight.className == "triangleRight") {
+        nextPage();
+    }
+})
 
 function modeSearch() {
     divSearch.className = "currentLinkTitle";
@@ -29,4 +52,70 @@ function modeSearch() {
     idInputSearch.style.display = "block";
     idInputMatch.style.display = "none";
     idAdvancedSearchPanel.style.transform = "rotateX(90deg)";
+    divResultContainer.style.marginRight = "0px";
+    idCurrentPointer.style.left = "252px";
+}
+
+function resultPartInitialization () {
+    try {
+        idResultPart1.style.opacity = "1";
+        idResultPart1.style.left = "0%";
+        idResultPart2.style.opacity = "1";
+        idResultPart2.style.left = "140%";
+        idResultPart3.style.opacity = "1";
+        idResultPart3.style.left = "280%";
+    } catch (e) {
+        console.log(e.message);
+    }
+    if ( logoNum <= 20 ) {
+        idTriangleRight.className = "triangleRightUnable";
+    }
+}
+
+function nextPage () {
+    if (idResultPart1.style.left == "0%") {
+        try {
+            idResultPart1.style.left = "-140%";
+            idResultPart2.style.left = "0%";
+            idResultPart3.style.left = "140%";
+        } catch (e) {
+            console.log(e.message);
+        }
+        
+        idTriangleLeft.className = "triangleLeft";
+        if (logoNum <= 40) {
+            idTriangleRight.className = "triangleRightUnable";
+        }
+    } else if (idResultPart1.style.left == "-140%") {
+        try {
+            idResultPart1.style.left = "-280%";
+            idResultPart2.style.left = "-140%";
+            idResultPart3.style.left = "0%";
+        } catch (e) {
+            console.log(e.message);
+        }
+        idTriangleRight.className = "triangleRightUnable";
+    }
+}
+
+function previousPage () {
+    if (idResultPart1.style.left == "-140%") {
+        try {
+            idResultPart1.style.left = "0%";
+            idResultPart2.style.left = "140%";
+            idResultPart3.style.left = "280%";
+        } catch (e) {
+            console.log(e.message);
+        }
+        idTriangleLeft.className = "triangleLeftUnable";
+    } else if (idResultPart1.style.left == "-280%") {
+        try {
+            idResultPart1.style.left = "-140%";
+            idResultPart2.style.left = "0%";
+            idResultPart3.style.left = "140%";
+        } catch (e) {
+            console.log(e.message);
+        }
+        idTriangleRight.className = "triangleRight";
+    }
 }
