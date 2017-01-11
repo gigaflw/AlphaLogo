dcc3e572b3fb9b8069ad31fb79f685c0a7a6b76a
GigaFlower <a_gigantic_flower@yeah.net>
2017-01-12 03:32:02 +0800

    Index page betterness

--- website/static/index.js
+++ website/static/index.js
@@ -2,7 +2,7 @@
 * @Author: GigaFlower
 * @Date:   2016-12-23 23:12:23
 * @Last Modified by:   GigaFlower
-* @Last Modified time: 2016-12-23 23:16:48
+* @Last Modified time: 2017-01-12 03:19:56
 */
 
 'use strict';
@@ -47,7 +47,6 @@ var advancedBrightnessHigh = document.getElementById("brightnessHigh");
 var advancedEnterpriseName = document.getElementById("advancedEnterpriseName");
 
 window.addEventListener('load', function(){
-    console.log('远看黄山黑黝黝,上面小来下面大,若将黄山倒过来,上面大来下面小');
     modeSearch();
     advancedSearchTypeInitialization();
 });
@@ -117,12 +116,12 @@ function switchAdvancedSearch() {
     if (divAdvancedSearch.className == "passiveAdvancedTitle") {
         idTypeSearch.value = "advancedSearch";
         divAdvancedSearch.className = "activeAdvancedTitle";
-        idTitle.className = "titleUp";
+        idTitle.classList.toggle("up");
         idAdvancedSearchPanel.style.transform = "rotateX(0deg)";
     } else {
         idTypeSearch.value = "search";
         divAdvancedSearch.className = "passiveAdvancedTitle";
-        idTitle.className = "title";
+        idTitle.classList.toggle("up");
         idAdvancedSearchPanel.style.transform = "rotateX(90deg)";
     }
 }
@@ -135,8 +134,8 @@ function advancedSearchTypeInitialization() {
     for (var x=0; x<advancedSearchTypeArray.length; ++x) {
         (function(){    // js的函数闭包问题
             var i = x;
-            advancedSearchTypeArray[i].addEventListener("click", function(){
-                switchAdvancedSearchType(this);
+            advancedSearchTypeArray[i].addEventListener("click", function(){;
+                this.classList.toggle('active')
                 if (i < 5) {
                     nColorsInput(i, this);
                 } else if (i < 8) {
@@ -148,14 +147,6 @@ function advancedSearchTypeInitialization() {
                 }
             });
         })();
-    }
-}
-
-function switchAdvancedSearchType(advancedSearchType) {
-    if (advancedSearchType.className == "advancedSearchType") {
-        advancedSearchType.className = "advancedSearchTypeActive";
-    } else {
-        advancedSearchType.className = "advancedSearchType";
     }
 }
 
